c0f1b4a5311f7f9e069178ce1e130e82c031a5c6
Писарев Вадим Сергеевич <v.pisarev@els24.com>
2025-02-22 02:07:07 +0700

    fix merge

--- src/navigation/layout.tsx
+++ src/navigation/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/header";
+import { Header } from "../components/header/header";
 import { Player } from "../components/player";
 import { MyProvider } from "../context";
 
